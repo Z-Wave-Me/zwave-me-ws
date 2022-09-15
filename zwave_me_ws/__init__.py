1db7f472af0841e79b2dc3d6d45220eda1ae6110
Dmitry Vlasov <kerbalspacema@gmail.com>
2022-09-15 13:16:20 +0300

    2.7 Physical Device Identifier

--- zwave_me_ws/__init__.py
+++ zwave_me_ws/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 from .helpers import ZWaveMeData
 from .ZWaveMe import ZWaveMe
--- zwave_me_ws/helpers.py
+++ zwave_me_ws/helpers.py
@@ -9,6 +9,8 @@ FIELDS = [
     "manufacturer",
     "firmware",
     "tags",
+    "creatorId",
+    "nodeId"
 ]
 METRICS_SCALE = ["title", "level", "scaleTitle", "min", "max", "color", "isFailed"]
 TYPE_LIGHT = "type-light"
@@ -20,6 +22,7 @@ class ZWaveMeData:
     deviceType: str
     title: str
     level: Union[str, int, float]
+    deviceIdentifier: str
     probeType: str = ""
     scaleTitle: str = ""
     min: str = ""
@@ -43,23 +46,33 @@ def prepare_devices(devices: list[dict]) -> list[ZWaveMeData]:
                 if key in device["metrics"]
             },
         }
-        if prepared_device["probeType"] == "siren":
-            prepared_device["deviceType"] = "siren"
-        elif TYPE_LIGHT in prepared_device["tags"] and prepared_device[
-            "deviceType"
-        ] not in ["switchRGB", "switchRGBW"]:
-            prepared_device["deviceType"] = "lightMultilevel"
-            prepared_device["color"] = {
-                "r": round(2.55 * float(prepared_device["level"])),
-                "g": round(2.55 * float(prepared_device["level"])),
-                "b": round(2.55 * float(prepared_device["level"])),
-            }
-            prepared_device["level"] = (
-                "on" if float(prepared_device["level"]) > 0 else "off"
-            )
-        elif prepared_device["probeType"] == "motor":
-            prepared_device["deviceType"] = "motor"
-        elif prepared_device["probeType"] == "fan":
-            prepared_device["deviceType"] = "fan"
+        prepared_device = set_device_type(prepared_device)
+        if "nodeId" in prepared_device:
+            prepared_device[
+                "deviceIdentifier"] = f"{prepared_device['creatorId']}_{prepared_device['nodeId']}"
+        else:
+            prepared_device["deviceIdentifier"] = prepared_device["id"]
         prepared_devices.append(prepared_device)
     return [ZWaveMeData(**d) for d in prepared_devices]
+
+
+def set_device_type(prepared_device):
+    if prepared_device["probeType"] == "siren":
+        prepared_device["deviceType"] = "siren"
+    elif TYPE_LIGHT in prepared_device["tags"] and prepared_device[
+        "deviceType"
+    ] not in ["switchRGB", "switchRGBW"]:
+        prepared_device["deviceType"] = "lightMultilevel"
+        prepared_device["color"] = {
+            "r": round(2.55 * float(prepared_device["level"])),
+            "g": round(2.55 * float(prepared_device["level"])),
+            "b": round(2.55 * float(prepared_device["level"])),
+        }
+        prepared_device["level"] = (
+            "on" if float(prepared_device["level"]) > 0 else "off"
+        )
+    elif prepared_device["probeType"] == "motor":
+        prepared_device["deviceType"] = "motor"
+    elif prepared_device["probeType"] == "fan":
+        prepared_device["deviceType"] = "fan"
+    return prepared_device
